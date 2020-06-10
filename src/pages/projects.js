a8b95e0a8819cf0da7e04df7357348b46440691c
Meir Taffel <artgardening@gmail.com>
2020-06-09 20:57:48 -0400

    Remove projects page

--- src/pages/projects.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import ProjectPage from "../components/Pages/Projects/ProjectPage";
-// import Layout from '../components/layout';
-
-// export default () => (
-//     <Layout>
-//         <ProjectPage/>
-//     </Layout>
-// );
