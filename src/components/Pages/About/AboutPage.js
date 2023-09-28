529d8963c8a34e88bc1c98bc605c9b30896a7992
Meyer <artgardening@gmail.com>
2023-09-28 14:35:52 -0400

    Put placeholder in about

--- src/components/Pages/About/AboutPage.js
+++ src/components/Pages/About/AboutPage.js
@@ -13,12 +13,14 @@ class AboutPage extends React.Component {
                 <CardPage title={"My Story:"
                     }>
                     <div className={styles.info}>
-                        <CardInfo text={"Hey! I'm Meyer. Add: Current studies/research and hobbies a bit of trivia."}
+                        <CardInfo text={"About coming soon."}
+                        />
+                        {/* <CardInfo text={"Hey! I'm Meyer. Add: Current studies/research and hobbies a bit of trivia."}
                             choice1={"Add 2020-2024 here. GSU/move to umd/current."}
                             choice2={"Add 2016-2020 here. Wekka/Freelancing/move to Atlanta/tree fall+covid/climbing"}
                             choice3={"Add 2019-2019 here. Uganda and getting involved in ACTS"}
                             choice4={"Add highschool-2016 here: After graduating highschool in 2012, I spent a year abroad that turned into 6, etc"}
-                        />
+                        /> */}
                         <ConnectInfo title={"That's all for now, stay tuned."}
                             email={"(And the adventure has only just begun.)"}
                             social1={"Upwork"}
