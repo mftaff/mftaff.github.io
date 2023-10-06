bbf561d0a0534e4ff67ff2e6654f6a3111fe9d33
Meyer <artgardening@gmail.com>
2023-10-06 16:09:03 -0400

    Add about page content

--- src/components/Pages/About/AboutPage.js
+++ src/components/Pages/About/AboutPage.js
@@ -10,10 +10,20 @@ class AboutPage extends React.Component {
     render() {
         return (
             <div className={styles.page}>
-                <CardPage title={"My Story:"
+                <CardPage title={"About Me:"
                     }>
                     <div className={styles.info}>
-                        <CardInfo text={"About coming soon."}
+                        <CardInfo text={"Hello!"}
+                            paragraphs={[
+                                "My name’s Meyer, I am a non-traditional undergraduate physics major in my final year at University of Maryland. I’ve returned to study physics to use applied physics R&D to contribute to transitioning to a sustainable world.",
+                                "Beginning Summer 2021, I’ve been an undergraduate researcher in applied physics / geophysics with Lathrop Labs. I conduct independent research and collaborate with my fellow lab members.",
+                                "My research role is primarily connected with the NSF grant - “Aerial Vehicle using AI for Landmine Detection” (AVAILD). I designed and fabricated the landing gear for our altaX drone (wingspan over 7ft!) as well as the payload integration for our sensor suite. This required extensive knowledge of Fusion360/autoCAD, machining, 3D-printing, and CFRP FDM.",
+                                "Now that we are up and flying (I’m an FAA-certified sUAS Pilot), I am working on data collection and analysis for near-surface anomaly/utility detection.",
+                                "In addition to my current research role, I also have a strong background (6+ yrs) in software development; including co-founding a tech startup and freelancing as a full-stack developer prior to my time here at UMD. I’m extremely experienced in Python, Matlab, JS, React and Rails.",
+                                "Apart from pursuing applied physics, I’m also the coordinator for ACTS Int'l, U.S. Chapter (a Ugandan non-profit). We aim to provide a brighter future for young Ugandans by connecting them with educational sponsors.",
+                                "When I’m not in the lab, you’ll typically find me out in the mountains rock climbing or exploring our wonderful world!",
+                                "I’m interested in opportunities in R&D towards sustainability or drone-based physics/geophysics."
+                            ]}
                         />
                         {/* <CardInfo text={"Hey! I'm Meyer. Add: Current studies/research and hobbies a bit of trivia."}
                             choice1={"Add 2020-2024 here. GSU/move to umd/current."}
@@ -21,12 +31,14 @@ class AboutPage extends React.Component {
                             choice3={"Add 2019-2019 here. Uganda and getting involved in ACTS"}
                             choice4={"Add highschool-2016 here: After graduating highschool in 2012, I spent a year abroad that turned into 6, etc"}
                         /> */}
-                        <ConnectInfo title={"That's all for now, stay tuned."}
-                            email={"(And the adventure has only just begun.)"}
+                        <ConnectInfo
+                            title={"Let's Connect!"}
+                            email={"mtaffel@umd.edu"}
                             social1={"Upwork"}
                             socialUrl1={"https://www.upwork.com/fl/meyertaffel"}
                             social2={<FaLinkedin />}
-                            socialUrl2={"https://www.linkedin.com/in/mftaff/"}/>
+                            socialUrl2={"https://www.linkedin.com/in/mftaff/"}
+                        />
                     </div>
                 </CardPage>
             </div>
