087ed30e23e8817886dce56fea6a51a0a9a42515
Meyer <artgardening@gmail.com>
2024-06-11 15:47:02 -0400

    add tracking to more pursuits button

--- src/components/Common/BorderedButton.js
+++ src/components/Common/BorderedButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import * as style from './BorderedButton.module.css'
 import {Link} from 'gatsby';
+import { trackCustomEvent } from 'gatsby-plugin-google-gtag'
 
 const BorderedButton = ({href, to, target, name}) => {
 
     if (to != null) {
         return (
-            <Link to={to} target={target} className={style.largeButton} data-ripple="true">
+            <Link to={to} target={target} className={style.largeButton} data-ripple="true" onClick={trackEvent}>
                 <h3 className={style.text}>
                     {name}
                 </h3>
@@ -22,5 +23,14 @@ const BorderedButton = ({href, to, target, name}) => {
         );
     }
 };
+
+function trackEvent() {
+    trackCustomEvent({
+        category: "Current Pursuits Button",
+        action: "Click",
+        // label: "Gatsby Plugin Example Campaign",
+        value: 1,
+    })
+}
 
 export default BorderedButton;
